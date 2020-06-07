89e8692262b0a46317fa2f46f57447eb27e3e16b
tsjrw <tesexbox360@hotmail.com>
2020-06-07 20:31:15 +0100

    mailgun-setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+
   host = 'pacific-reef-58744.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => 465,
-    :authentication => :plain,
-    :user_name      => Rails.application.credentials.sendgrid[:user_name],
-    :password       => Rails.application.credentials.sendgrid[:password],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true,
-    tls: true
-  }
+  config.action_mailer.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'heroku.com',
+  :authentication => :plain,
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
